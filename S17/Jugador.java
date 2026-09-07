452e2178c89a20135d3c3c8aae7f6763d6ac0436
Eloy-Garcia-Ramirez <eloygarcia.dgps@gmail.com>
2026-09-07 16:22:51 +0000

    GUARDAR

---
+++ S17/Jugador.java
@@ -0,0 +1,19 @@
+package S17;
+
+public class Jugador {
+    // Atibuto
+    private String nJugador;
+
+
+    // Constructor
+    public Jugador(String nJugador) {
+        this.nJugador = nJugador;
+    }
+
+
+    // Método Getter
+    public String getNJugador() {
+        return nJugador;
+    }
+}
+
